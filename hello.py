999c348d523c71c3f24ced67563bd1bed19cf92b
Imran Siddique <imrny@yahoo.com>
2018-11-14 10:55:19 -0500

    multiply func

--- hello.py
+++ hello.py
@@ -3,6 +3,10 @@ print("Hello World!")
 def add_int(x, y):
     return x + y
 
+def  mul_int(x, y, z):
+    return x*y*z
+
 int_sum = add_int(3, 6)
 print(int_sum)
 print('The sum of 3 and 6 is {}'.format(int_sum))
+print('The product is: ',  mul_int(3,4,5))
